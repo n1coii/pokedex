c40c4bbf6cacee47698714de2a9651fab5ad1f08
Nicolas <nicolaslegros98@gmail.com>
2021-03-01 21:19:26 -0500

    PokemonInfo works

--- components/pokemonInfo.tsx
+++ components/pokemonInfo.tsx
@@ -12,7 +12,7 @@ const PokemonInfo = ({ route, navigation }) => {
     // let DATA = [poke.types, poke.moves];
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/1/')
+        fetch(url)
           .then((response) => response.json())
         //   .then(response => console.log("RESPONSE: ", response))
           .then((response) => setPoke(response))
